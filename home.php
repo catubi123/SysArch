ab14aae70779c41d92f8efdd5d3a5277c95d540d
Catubig_Mark_Dave_C <catubigmarkdave0@gmail.com>
2025-02-07 09:14:19 +0800

    Add user authentication system with login and registration functionality

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+    include('db.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>home</title>
+    <link rel="stylesheet" href="w3.css">
+</head>
+<body>
+    <h1>WELCOME USER</h1>
+    <button><a href="index.php">Logout</a></button>
+</body>
+</html>
